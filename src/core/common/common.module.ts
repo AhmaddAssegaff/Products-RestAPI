e2f5bfc1ac3cbfb1c354a0909dc48115d15c7e74
AhmaddAssegaff <139890680+AhmaddAssegaff@users.noreply.github.com>
2025-05-20 21:24:42 +0700

    refactor : install husky & type orm

--- src/core/common/common.module.ts
+++ src/core/common/common.module.ts
@@ -1,11 +1,10 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import Configs from '@config/index';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from 'joi';
 
 @Module({
-  controllers: [],
-  providers: [],
   imports: [
     ConfigModule.forRoot({
       load: Configs,
@@ -14,18 +13,37 @@ import * as Joi from 'joi';
       envFilePath: ['.env'],
       expandVariables: true,
       validationSchema: Joi.object({
-        // app config
-        PORT: Joi.number().default('3000').required(),
+        APP_PORT: Joi.number().required(),
         API_PREFIX: Joi.string().required(),
         ENABLE_VERSION: Joi.boolean().required(),
         VERSION_PREFIX: Joi.string().required(),
         DEFAULT_VERSION: Joi.string().required(),
         TZ: Joi.string().required(),
 
-        // swagger config
+        DB_NAME: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_HOST: Joi.string().hostname().required(),
+        DB_PORT: Joi.number().port().required(),
+
         SW_USERNAME: Joi.string().default('nest').required(),
         SW_PASSWORD: Joi.string().default('password').required(),
         SW_PATH: Joi.string().default('/docs').required(),
+      }),
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('databse.DB_HOST'),
+        port: configService.get<number>('database.DB_PORT'),
+        username: configService.get<string>('database.DB_USER'),
+        password: configService.get<string>('database.DB_PASS'),
+        database: configService.get<string>('database.DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // Hanya untuk dev
       }),
     }),
   ],
